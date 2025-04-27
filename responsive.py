27a34809e4df0898dabe7155cc6d46e615bc0a3a
Qudratillo Salokhiddinov <pythondarslar@gmail.com>
2025-04-27 17:01:23 +0500

    Fix auto check

--- responsive.py
+++ responsive.py
@@ -8,7 +8,8 @@ class MediaQueryManager:
         }
         self.current_break_point = None
     def on_resized(self,event:flet.WindowResizeEvent):
-        width = event.width
+        self.check_for_updates(event.width)
+    def check_for_updates(self,width):
         for name,(min_width,max_width) in self.breakpoints.items():
             if max_width >= width > min_width:
                 if self.current_break_point != name:
@@ -26,10 +27,10 @@ class MediaQuery:
         event.page._media_query.on_resized(event)
     def on(self,point,callback_function):
         self.page._media_query.listeners[point].append(callback_function)
+        self.page._media_query.check_for_updates(self.page.width)
     def off(self,point,callback_function):
         self.page._media_query.listeners[point].remove(callback_function)
         
     def register(self,point,min_width,max_width):
         self.page._media_query.breakpoints[point] = (min_width,max_width)
         self.page._media_query.listeners[point] = []
-        
